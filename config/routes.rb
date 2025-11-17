44b6beb25c982d48c5d904a4431ed5ff11d33f47
muranana7 <23261088@kyoto-wu.ac.jp>
2025-11-17 13:25:32 +0000

    Fix: 全ての修正（routes,login.html.erb）をまとめてコミット

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,9 @@ Rails.application.routes.draw do
   root "static_pages#login"
 
   # 各ページへのルート（名前付きヘルパー付き）
-  get "static_pages/login_new",  to: "static_pages#login_new",  as: :login_new
-  get "static_pages/login_pass", to: "static_pages#login_pass", as: :login_pass
+  get "static_pages/login",      to: "static_pages#login",      as: :static_pages_login
+  get "static_pages/login_new",  to: "static_pages#login_new",  as: :static_pages_login_new
+  get "static_pages/login_pass", to: "static_pages#login_pass", as: :static_pages_login_pass
 
   # ヘルスチェック
   get "up" => "rails/health#show", as: :rails_health_check
