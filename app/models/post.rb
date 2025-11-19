82da0132dc1c5d1ddf5b20e4a31d60f93f8299b6
torimomo <160314077+torimomo@users.noreply.github.com>
2025-11-19 14:41:19 +0000

    6th

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,6 @@
 class Post < ApplicationRecord
   # Active Storage: 複数の画像を添付できるように設定
-  has_many_attached :images 
+  has_many_attached :images
 
   # 本文 (content) は必須とする
   validates :content, presence: true
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
-#  get "posts/index"
-#  get "posts/show"
-#  get "posts/new"
-#  get "posts/create"
-#  get "posts/edit"
-#  get "posts/update"
-#  get "posts/destroy"
-  
+  #  get "posts/index"
+  #  get "posts/show"
+  #  get "posts/new"
+  #  get "posts/create"
+  #  get "posts/edit"
+  #  get "posts/update"
+  #  get "posts/destroy"
+
   resources :posts
   # ルート（トップページ）
   root "static_pages#login"
