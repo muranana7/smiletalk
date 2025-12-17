00d783b0a6b1678fb013625170beeadc6dbefad5
muranana7 <23261088@kyoto-wu.ac.jp>
2025-12-17 04:19:24 +0000

    testのエラー修正

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -4,8 +4,8 @@ require "securerandom"
 class UsersControllerTest < ActionDispatch::IntegrationTest
   setup do
     @user = users(:one)
-    # session[:user_id] を作るためにログイン
     post static_pages_login_path, params: { nickname: @user.nickname, password: "password" }
+    assert_response :success
   end
 
   test "should create user" do
@@ -14,19 +14,21 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
       user: {
         nickname: "テストユーザー_#{unique_suffix}",
         email: "test_#{unique_suffix}@example.com",
-        password: "password"
+        password: "password",
+        password_confirmation: "password"
       }
     }
     assert_redirected_to root_path
-    assert_equal "新規登録が完了しました", flash[:notice]
+    follow_redirect!
+    assert_select "div.alert-success", "新規登録が完了しました"
   end
 
   test "should update password" do
     patch update_password_path, params: {
-      password: "newpassword",
-      password_confirmation: "newpassword"
+      user: { password: "newpassword", password_confirmation: "newpassword" }
     }
     assert_redirected_to root_path
-    assert_equal "パスワードを更新しました", flash[:notice]
+    follow_redirect!
+    assert_select "div.alert-success", "パスワードを更新しました"
   end
 end
