566e7b5de72240a06a6f7fd5f02870b3e7881dee
torimomo <160314077+torimomo@users.noreply.github.com>
2025-12-26 01:52:43 +0000

    add delete

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,6 @@
 class PostsController < ApplicationController
   def index
+    @posts = Post.all
   end
 
   def show
@@ -9,7 +10,26 @@ class PostsController < ApplicationController
   def new
   end
 
-  def create
+  def create_post
+    @post = Post.new(
+      content: params[:content],
+      image: params[:image],
+      user: current_user   # ← これが必要！
+    )
+    @post.save
+    redirect_to static_pages_thread_view_path(id: @post.id)
+  end
+
+
+  def destroy
+    @post = Post.find(params[:id])
+
+    if @post.user == current_user
+      @post.destroy
+      redirect_to static_pages_index_path, notice: "投稿を削除しました"
+    else
+      redirect_to static_pages_index_path, alert: "削除できません"
+    end
   end
 
   def edit
@@ -18,6 +38,4 @@ class PostsController < ApplicationController
   def update
   end
 
-  def destroy
-  end
 end
