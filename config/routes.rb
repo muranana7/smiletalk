069b999f513e1dd08b7b207cbce53e1f8423beb5
shinoharamanami <23261055@kyoto-wu.ac.jp>
2026-01-01 05:09:41 +0000

    Rubocop指摘の修正（空白・配列表記）

--- config/routes.rb
+++ config/routes.rb
@@ -2,32 +2,25 @@ Rails.application.routes.draw do
   # ルート
   root "static_pages#login"
 
-  # StaticPagesController
-  get  "static_pages/login", to: "static_pages#login", as: :static_pages_login
-  post "static_pages/login", to: "static_pages#login_post", as: :login
+  # StaticPages
+  get  "static_pages/login",        to: "static_pages#login",        as: :static_pages_login
+  post "static_pages/login",        to: "static_pages#login_post",   as: :login
+  get  "static_pages/login_new",    to: "static_pages#login_new"
+  get  "static_pages/login_pass",   to: "static_pages#login_pass"
 
-  get "static_pages/login_new",  to: "static_pages#login_new"
-  get "static_pages/login_pass", to: "static_pages#login_pass"
-
-  get  "static_pages/thread_view",    to: "static_pages#thread_view"
-  get  "static_pages/thread_index",   to: "static_pages#thread_index", as: :static_pages_index
-  get  "static_pages/new_post",        to: "static_pages#new_post"
-  post "static_pages/create_post",     to: "static_pages#create_post"
-
-  get  "static_pages/post_edit",       to: "static_pages#post_edit"
-  post "static_pages/update",          to: "static_pages#update_post"
-  get  "static_pages/update_complete", to: "static_pages#update_complete"
-
-  post "static_pages/delete",          to: "static_pages#delete_post"
-  get  "static_pages/delete_complete", to: "static_pages#delete_complete"
+  get  "static_pages/thread_index", to: "static_pages#thread_index", as: :static_pages_index
+  get  "static_pages/thread_view",  to: "static_pages#thread_view"
 
   # Users
   resources :users, only: [:create]
-  patch "users/update_password", to: "users#update_password"
+  patch "users/update_password", to: "users#update_password", as: :update_password
 
-  # ✅ Posts（ここが最重要）
-  resources :posts, only: [:index, :show, :new, :create, :edit, :update, :destroy]
+  # Posts + Replies（重要）
+  resources :posts do
+    resources :replies, only: [:create]
+  end
 
   # ヘルスチェック
   get "up" => "rails/health#show", as: :rails_health_check
 end
+
