5920736d597a62f7588c325aa3482b34f3a87b1c
shinoharamanami <23261055@kyoto-wu.ac.jp>
2026-01-20 10:31:02 +0000

    削除のついか

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,22 +2,30 @@ class PostsController < ApplicationController
   before_action :set_post, only: [:show, :edit, :update, :destroy]
   before_action :authorize_user!, only: [:edit, :update, :destroy]
 
+  # 未ログイン時のアクセス制御
+  before_action :require_login, only: [:new, :create]
+
   def index
     @posts = Post.all
   end
 
   def show
+    @post = Post.find(params[:id])
   end
 
   def new
-    @post = Post.new
+    # require_login で未ログインは弾かれるので、そのままでOK
   end
 
-  def create
-    @post = current_user.posts.build(post_params)
+  def create_post
+    @post = Post.new(
+      content: params[:content],
+      image: params[:image],
+      user: current_user
+    )
 
     if @post.save
-      redirect_to static_pages_thread_view_path(id: @post.id), notice: "投稿しました"
+      redirect_to static_pages_thread_view_path(id: @post.id)
     else
       render :new
     end
@@ -27,17 +35,8 @@ class PostsController < ApplicationController
     render "static_pages/edit"
   end
 
-  def update
-    if @post.update(post_params)
-      redirect_to static_pages_thread_view_path(id: @post.id), notice: "更新しました"
-    else
-      render "static_pages/edit"
-    end
-  end
-
   def destroy
     @post = Post.find(params[:id])
-    # 自分の投稿だけ削除できるようにする
     unless @post.user_id == current_user.id
       redirect_to post_path(@post), alert: "権限がありません"
       return
@@ -45,6 +44,14 @@ class PostsController < ApplicationController
 
     @post.destroy
     redirect_to static_pages_index_path, notice: "削除しました"
+  end
+
+  def update
+    if @post.update(post_params)
+      redirect_to static_pages_thread_view_path(id: @post.id), notice: "更新しました"
+    else
+      render "static_pages/edit"
+    end
   end
 
   private
@@ -59,5 +66,11 @@ class PostsController < ApplicationController
 
   def post_params
     params.require(:post).permit(:title, :content, :image)
+  end
+
+  def require_login
+    unless logged_in?
+      redirect_to static_pages_index_path
+    end
   end
 end
