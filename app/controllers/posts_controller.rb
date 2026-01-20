ef76aca96e59cf9521ee0bcda5c2f11130c5fc29
shinoharamanami <23261055@kyoto-wu.ac.jp>
2026-01-20 10:06:30 +0000

    削除のついか

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,74 +1,63 @@
 class PostsController < ApplicationController
-  before_action :require_login, only: [ :new, :create, :edit, :update, :destroy ]
-  before_action :set_post, only: [ :show, :edit, :update, :destroy ]
-  before_action :authorize_user, only: [ :edit, :update, :destroy ]
+  before_action :set_post, only: [:show, :edit, :update, :destroy]
+  before_action :authorize_user!, only: [:edit, :update, :destroy]
 
-  # GET /posts
   def index
     @posts = Post.all
   end
 
-  # GET /posts/:id
   def show
   end
 
-  # GET /posts/new
   def new
     @post = Post.new
   end
 
-  # POST /posts
   def create
     @post = current_user.posts.build(post_params)
+
     if @post.save
-      redirect_to @post, notice: "投稿しました"
+      redirect_to static_pages_thread_view_path(id: @post.id), notice: "投稿しました"
     else
       render :new
     end
   end
 
-  # GET /posts/:id/edit
   def edit
+    render "static_pages/edit"
   end
 
-  # PATCH/PUT /posts/:id
   def update
     if @post.update(post_params)
-      redirect_to @post, notice: "更新しました"
+      redirect_to static_pages_thread_view_path(id: @post.id), notice: "更新しました"
     else
-      render :edit
+      render "static_pages/edit"
     end
   end
 
-  # DELETE /posts/:id
   def destroy
+    @post = Post.find(params[:id])
+    # 自分の投稿だけ削除できるようにする
+    unless @post.user_id == current_user.id
+      redirect_to post_path(@post), alert: "権限がありません"
+      return
+    end
+
     @post.destroy
-    redirect_to posts_url, notice: "削除しました"
+    redirect_to static_pages_index_path, notice: "削除しました"
   end
 
   private
 
-  # ログインしていなければリダイレクト
-  def require_login
-    unless current_user
-      redirect_to static_pages_index_path, alert: "ログインしてください"
-    end
-  end
-
-  # 投稿を取得
   def set_post
     @post = Post.find(params[:id])
   end
 
-  # 投稿者以外は編集・削除不可
-  def authorize_user
-    unless @post.user == current_user
-      redirect_to posts_path, alert: "権限がありません"
-    end
+  def authorize_user!
+    redirect_to static_pages_index_path, alert: "権限がありません" unless @post.user == current_user
   end
 
-  # Strong Parameters
   def post_params
-    params.require(:post).permit(:title, :content)
+    params.require(:post).permit(:title, :content, :image)
   end
 end
