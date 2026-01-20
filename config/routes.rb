df53a59e91f667c159e85faa2e699f72542ca5fd
shinoharamanami <23261055@kyoto-wu.ac.jp>
2026-01-20 09:58:20 +0000

    削除のついか

--- config/routes.rb
+++ config/routes.rb
@@ -1,28 +1,30 @@
 Rails.application.routes.draw do
-  # ルート
   root "static_pages#login"
 
   # StaticPages
-  get  "static_pages/login", to: "static_pages#login",      as: :static_pages_login
-  post "static_pages/login", to: "static_pages#login_post", as: :login
-
-  get "static_pages/login_new",  to: "static_pages#login_new"
-  get "static_pages/login_pass", to: "static_pages#login_pass"
-
-  get  "static_pages/thread_view",  to: "static_pages#thread_view",  as: :static_pages_thread_view
+  get  "static_pages/login",       to: "static_pages#login",       as: :static_pages_login
+  post "static_pages/login",       to: "static_pages#login_post",  as: :login
+  get  "static_pages/login_new",   to: "static_pages#login_new",   as: :static_pages_login_new
+  get  "static_pages/login_pass",  to: "static_pages#login_pass",  as: :static_pages_login_pass
+  get  "static_pages/thread_view", to: "static_pages#thread_view", as: :static_pages_thread_view
+  get  "static_pages/post_edit",   to: "static_pages#post_edit",   as: :static_pages_post_edit
+  post "static_pages/delete",      to: "static_pages#delete_post", as: :static_pages_delete_post
+  get  "static_pages/delete_complete", to: "static_pages#delete_complete", as: :static_pages_delete_complete
+  post "static_pages/update",      to: "static_pages#update_post", as: :static_pages_update_post
+  get  "static_pages/update_complete", to: "static_pages#update_complete", as: :static_pages_update_complete
   get  "static_pages/thread_index", to: "static_pages#thread_index", as: :static_pages_index
-  get  "static_pages/new_post",     to: "static_pages#new_post"
-  post "static_pages/create_post",  to: "static_pages#create_post"
+  get  "static_pages/new_post",    to: "static_pages#new_post",    as: :static_pages_new_post
+  post "static_pages/create_post", to: "static_pages#create_post", as: :static_pages_create_post
 
   # Users
-  resources :users, only: [:create]
-  patch "users/update_password", to: "users#update_password"
+  resources :users, only: [ :create ]
+  patch "users/update_password", to: "users#update_password", as: :update_password
 
-  # Posts（★ここが重要）
-  resources :posts, only: [:show, :edit, :update, :destroy] do
+  # Posts & Replies
+  resources :posts do
     resources :replies, only: :create
   end
 
-  # ヘルスチェック
-  get "up" => "rails/health#show"
+  # Health check
+  get "up", to: "rails/health#show", as: :rails_health_check
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2026_01_16_031222) do
+ActiveRecord::Schema[8.0].define(version: 2026_01_20_094128) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -55,6 +55,7 @@ ActiveRecord::Schema[8.0].define(version: 2026_01_16_031222) do
     t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "image"
     t.index ["post_id"], name: "index_replies_on_post_id"
   end
 
