1ab6949e83d9ed9457678c03fe2a1a9bce3e9b58
shinoharamanami <23261055@kyoto-wu.ac.jp>
2026-01-23 05:24:03 +0000

    Fix

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,15 +1,18 @@
+
+# app/controllers/posts_controller.rb
 class PostsController < ApplicationController
-  before_action :require_login, only: [ :new, :create, :edit, :update, :destroy ]
-  before_action :set_post, only: [ :show, :edit, :update, :destroy ]
-  before_action :authorize_user, only: [ :edit, :update, :destroy ]
+  before_action :require_login, only: %i[new create edit update destroy]
+  before_action :set_post,      only: %i[show edit update destroy]
+  before_action :authorize_user!, only: %i[edit update destroy]
 
   # GET /posts
   def index
-    @posts = Post.all
+    @posts = Post.order(created_at: :desc)
   end
 
   # GET /posts/:id
   def show
+    # @post は set_post で取得済み
   end
 
   # GET /posts/new
@@ -20,52 +23,53 @@ class PostsController < ApplicationController
   # POST /posts
   def create
     @post = current_user.posts.build(post_params)
-
     if @post.save
       redirect_to @post, notice: "投稿しました"
+      # 例: redirect_to static_pages_thread_view_path(id: @post.id), notice: "投稿しました"
     else
-      render :new
+      render :new, status: :unprocessable_entity
     end
   end
 
   # GET /posts/:id/edit
   def edit
-    render "static_pages/edit"
+    # 例: render "static_pages/edit"
   end
 
   # PATCH/PUT /posts/:id
   def update
     if @post.update(post_params)
-      redirect_to static_pages_index_path, notice: "更新しました"
+      redirect_to @post, notice: "更新しました"
+      # 例: redirect_to static_pages_thread_view_path(id: @post.id), notice: "更新しました"
     else
-      render "static_pages/edit"
+      render :edit, status: :unprocessable_entity
     end
   end
 
   # DELETE /posts/:id
   def destroy
     @post.destroy
-    redirect_to static_pages_index_path, notice: "削除しました"
+    redirect_to posts_url, notice: "削除しました"
+    # 例: redirect_to static_pages_index_path, notice: "削除しました"
   end
 
   private
 
-  # ログイン必須
-  def require_login
-    unless logged_in?
-      redirect_to static_pages_index_path, alert: "ログインしてください"
-    end
-  end
-
-  # 投稿取得
   def set_post
     @post = Post.find(params[:id])
   end
 
-  # 投稿者本人のみ許可
-  def authorize_user
-    unless @post.user == current_user
-      redirect_to posts_path, alert: "権限がありません"
+  # 投稿者以外は編集・削除不可
+  def authorize_user!
+    redirect_to posts_path, alert: "権限がありません" unless @post.user_id == current_user&.id
+  end
+
+  # 未ログイン時のアクセス制御
+  def require_login
+    unless current_user
+      redirect_to login_path, alert: "ログインしてください"
+      # ↑ アプリで使っているログインページのパスに置き換えてください
+      # 例: redirect_to static_pages_index_path, alert: "ログインしてください"
     end
   end
 
