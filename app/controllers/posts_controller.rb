58941552eea3b020e7b6f781f0cc87151abbe6dc
shinoharamanami <23261055@kyoto-wu.ac.jp>
2026-01-27 05:57:34 +0000

    削除と編集

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,41 +1,50 @@
 class PostsController < ApplicationController
+  before_action :require_login, only: [:new, :create, :edit, :update, :destroy]
+  before_action :set_post, only: [:show, :edit, :update, :destroy]
+
   def index
     @posts = Post.all
   end
- 
+
   def show
-    @post = Post.find(params[:id])
   end
- 
+
   def new
+    @post = Post.new
   end
- 
-  def create_post
-    @post = Post.new(
-      content: params[:content],
-      image: params[:image],
-      user: current_user
-    )
-    @post.save
-    redirect_to static_pages_thread_view_path(id: @post.id)
-  end
- 
- 
-  def destroy
-    @post = Post.find(params[:id])
- 
-    if @post.user == current_user
-      @post.destroy
-      redirect_to static_pages_index_path, notice: "投稿を削除しました"
+
+  def create
+    @post = current_user.posts.build(post_params)
+    if @post.save
+      redirect_to @post
     else
-      redirect_to static_pages_index_path, alert: "削除できません"
+      render :new, status: :unprocessable_entity
     end
   end
- 
+
   def edit
+    redirect_to posts_path unless @post.user == current_user
   end
- 
+
   def update
+    redirect_to posts_path unless @post.user == current_user
+    @post.update(post_params)
+    redirect_to @post
+  end
+
+  def destroy
+    redirect_to posts_path unless @post.user == current_user
+    @post.destroy
+    redirect_to posts_path
+  end
+
+  private
+
+  def set_post
+    @post = Post.find(params[:id])
+  end
+
+  def post_params
+    params.require(:post).permit(:content, :image)
   end
 end
- 
